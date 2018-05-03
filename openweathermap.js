35ebfdc49b6ad4b1dc56da77e7c682af6538064d
Mark Fitzpatrick <markfitz815@gmail.com>
2018-05-03 18:20:24 +0100

    added application div with weather div and header inside, created styles page and styled weatherHead

--- openweathermap.js
+++ openweathermap.js
@@ -1,9 +1,11 @@
 
 let info = {
-    initTime : ''
+    initTime : '',
+    lat: '',
+    lon: ''
 }
 
-let requestURL = 'http://api.openweathermap.org/data/2.5/weather?q=Dublin&APPID=48e411ef7b37e3322417098d6f26293b&units=metric';
+let requestURL;
 let request = new XMLHttpRequest();
 
 
@@ -13,6 +15,9 @@ function getGeo(){ navigator.geolocation.getCurrentPosition(function(e){
     let myTime = new Date();
     myTime.getDate(e.timestamp)
     info.initTime = myTime;
+    info.lat = Math.floor(e.coords.latitude);
+    info.lon = Math.floor(e.coords.longitude);
+    requestURL = 'http://api.openweathermap.org/data/2.5/weather?lat='+info.lat+'&lon='+info.lon+'&APPID=48e411ef7b37e3322417098d6f26293b&units=metric';
     if(e){gotLoc()}
     }, function(err){initFail();}
     );
@@ -43,13 +48,16 @@ function gotLoc(){
     request.open('GET', requestURL);
     request.responseType = 'json';
     request.send();
+    console.log(info.lat);
     reqTime();
 }
 
+//
 let reqTime = function(){
     setTimeout(function(){
         if(request.response){
             console.log(request.response);
+            info.city = request.response.sys.country;
             initApp();
         }else{
             console.log('not yet');
@@ -59,7 +67,25 @@ let reqTime = function(){
 }
 
 
+//creates app when permission is granted
+function initApp(){
+
+    let appDiv = document.createElement('div');
+    appDiv.id="app";
+    document.body.appendChild(appDiv);
+
+    let weatherDiv = document.createElement('div');
+    weatherDiv.classList += 'weatherDiv';
+    appDiv.appendChild(weatherDiv);
+
+    let weatherHead = document.createElement('div');
+    weatherHead.classList += 'weatherHead';
+    weatherHead.innerHTML = info.city;
+    weatherDiv.appendChild(weatherHead);
+}
 
 
 
+
+//initializes permission to get geolocation
 getGeo();
